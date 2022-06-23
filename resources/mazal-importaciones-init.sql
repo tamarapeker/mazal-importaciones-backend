6f5fca46e46e860e451fbcd3a9becb3ea51cfb50
TSOFT\Tamara.Peker <tamara.peker@tsoftlatam.com>
2022-06-23 16:22:25 -0300

    init data

--- resources/mazal-importaciones-init.sql
+++ resources/mazal-importaciones-init.sql
@@ -1,29 +1,35 @@
+create table category
+(
+    category_id SERIAL primary key,
+	category_name varchar(100) not null unique
+);
+
 create table subcategory 
 (
 	subcategory_id SERIAL primary key,
-	subcategory_name text not null unique,
+	subcategory_name varchar(100) not null unique,
     category_id int references category(category_id)
 );
 
 create table product
 (
 	product_id SERIAL primary key,
-    product_code text not null unique,
-	product_name text not null,
-    product_description text,
-    product_measures json,
-    product_unit text,
-    product_min_cant text,
-    product_cant_bulto text,
-    product_image bytea,
-    product_observations text,
+    product_code varchar(100) not null unique,
+	product_name varchar(100) not null,
+    product_description varchar(100),
+    product_measures longtext,
+    product_unit varchar(100),
+    product_min_cant varchar(100),
+    product_cant_bulto varchar(100),
+    product_image longblob,
+    product_observations varchar(100),
     product_timestamp date default current_date not null
 );
 
 create table product_category_subcategory
 (
-    product_category_subcategory_id SERIAL primary key,
-    product_id int references product(product_id) not null,
-    category_id int references category(category_id) not null,
-    subcategory_id int references subcategory(subcategory_id) not null
+    prod_cat_subcat_id SERIAL primary key,
+    product_id int references product(product_id),
+    category_id int references category(category_id),
+    subcategory_id int references subcategory(subcategory_id)
 );
